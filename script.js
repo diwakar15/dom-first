36f5d1b5135e8428c92aae59b3ced0f09dc427ad
diwakar15 <30283643+diwakar15@users.noreply.github.com>
2022-10-12 11:41:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,43 @@
-// request.response
-// var request=new XMLHttpRequest();
-// request.open("GET","https://restcountries.com/v2/all");
-// request.send();
-// request.onload=function()
-// {
-//     var res=JSON.parse(request.response);
-//     console.log(res);
-//     for(i=0;i<res.length;i++)
-//     {
-//         console.log(res[i].name,res[i].region,res[i].subregion);
-//     }
-// };
 
 
+function createlabels(tagname,attrname,attrvalue,content){
+var ele=document.createElement(tagname);
+ele.setAttribute(attrname,attrvalue);
+ele.innerHTML=content;
+return ele;
+}
+//it is returning an HTML element
+//<label for="email">Mail</label>
 
-// var request=new XMLHttpRequest();
-// request.open("GET","https://restcountries.com/v2/all");
-// request.send();
-// request.onload=function()
-// {
-//     var res=JSON.parse(request.response);
-//     console.log(res);
-//     for(i=0;i<res.length;i++)
-//     {
-//         console.log("contry name is" +res[i].name,"captail is"+res[i].capital);
-//     }
-// };
+function createinput(tagname,attrname,attrvalue,attrname1,attrvalue1){
+    var ele=document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.setAttribute(attrname1,attrvalue1);
+    return ele;
+}
+function createlinebreak(tagname){
+var ele=document.createElement(tagname);
+return ele;
+}
+var label=createlabels("label","for","fname","firstname");
+var br1=createlinebreak("br");
+var input=createinput("input","type","firstname","id","fname");
+var br2=createlinebreak("br");
+
+var middlename=createlabels("label","for","mname","middlename");
+var br3=createlinebreak("br");
+var minput=createinput("input","type","middlename","id","mname");
+var br4=createlinebreak("br");
 
 
-var request=new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v2/all");
-request.send();
-request.onload=function()
-{
-    var res=JSON.parse(request.response);
-    console.log(res);
-    
-    // var coun =res.filter((ele)=>ele.region=="europe");
-    // console.log(coun);
-    // var result=coun.map((ele)=>ele.name);
-    // console.log(result);
-    //  var coun =res.filter((ele)=>ele.region=="Europe").map((ele)=>ele.name);
-    //  console.log(coun);
-    var
-};
+var lastname=createlabels("label","for","lname","lastname");
+var br5=createlinebreak("br");
+var linput=createinput("input","type","lastname","id","lname");
+var br6=createlinebreak("br");
+
+
+var phonenumber=createlabels("label","for","phn","phonenumber");
+var br7=createlinebreak("br");
+var phoneinput=createinput("input","type","phonenumber","id","phn");
+var br8=createlinebreak("br");
+document.body.append(label,br1,input,br2,middlename,br3,minput,br4,lastname,br5,linput,br6,phonenumber,br7,phoneinput,br8);
